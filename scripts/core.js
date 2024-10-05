9ebba61f7a5e460d743b1eb4ada311cd00cfc555
Corbeau <aurora.griffith@students.mq.edu.au>
2024-10-05 20:10:18 +1000

    the padding is being strange but it's fine

--- scripts/core.js
+++ scripts/core.js
@@ -43,6 +43,7 @@ function day_of_year_hash(quote_number){
     let day_stuff = Math.pow(parseInt(now.getDate()),3);
     let month_stuff = Math.pow(parseInt(now.getMonth()),2);
     let year_stuff = parseInt(now.getFullYear());
+    // hash salt is just that randomness value, it should just be 0 if we're not including randomness
     let hash_salt = (include_randomness)? randomness_salting(quote_number) : 0;
     return (day_stuff + month_stuff + year_stuff + hash_salt) % quote_number;
 }
@@ -61,11 +62,13 @@ function get_quote_daily(source_index){
     let hash_val = get_todays_hash(overall_count);
     // the amount of indexing left to do
     let indexing_left = parseInt(hash_val);
-    // current book index
-    let book_index = 0;
+    // current book index and quote
     let book_and_verse = [0,0];
     // the number of books
     const current_book_count = DB.sources[source_index].books[book_and_verse[0]];
+    // keep track of if it was a real one
+    //  might be a strange index if there was a bug in the hashing somewhere
+    let found_real_index = false;
     // try find the book and use it
     while(book_and_verse[0] < DB.sources[source_index].books.length){
         // shorter names
@@ -79,10 +82,20 @@ function get_quote_daily(source_index){
         // actually found?
         else {
             book_and_verse[1] = indexing_left;
+            found_real_index = true;
             break;
         }
     }
-    // if we didnt find one? suffer?
+    // possibly didnt find real index if spooky things happen
+    // if we didnt find one? suffer? bless them?
+    if(!found_real_index) {
+        // randomize the book index
+        //  modulo the number of books incase someones javascript compiler wants to round up in integer parsing
+        book_and_verse[0] = parseInt((Math.random*(DB.sources[source_index].books.length))%DB.sources[source_index].books.length);
+        // again for the verse
+        //  this is suffering to make and suffering to read, dont do this.
+        book_and_verse[1] = parseInt((Math.random*(DB.sources[source_index].books[book_and_verse[0]].content.quotes.length))%DB.sources[source_index].books[book_and_verse[0]].content.quotes.length);
+    }
     // console.log(`was ${book_and_verse[0]} and ${book_and_verse[1]}`);
     return {
         "book": book_and_verse[0],
