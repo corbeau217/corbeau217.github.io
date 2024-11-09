23b36beef3787bf06fe407173736db443cfab9ab
Corbeau <aurora.griffith@students.mq.edu.au>
2024-11-10 08:40:59 +1100

    quick cheeky data update

---
+++ data/daily_post_data.js
@@ -0,0 +1,110 @@
+const DAILY_POST_DATA = {
+    "post_meta": [
+        // ========================================
+        // ========================================
+        // ===== template for post data ===========
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/12/31",
+            // the theme that post is for
+            "theme": "lorem ipsum",
+            // the day that post is for
+            "day": "lorem ipsum",
+            // the name for the post on that day
+            "title": "lorem ipsum",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/11/10",
+            // the theme that post is for
+            "theme": "situation",
+            // the day that post is for
+            "day": "sunday",
+            // the name for the post on that day
+            "title": "lorem ipsum",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/11/09",
+            // the theme that post is for
+            "theme": "cleanup",
+            // the day that post is for
+            "day": "saturday",
+            // the name for the post on that day
+            "title": "post importing",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/11/08",
+            // the theme that post is for
+            "theme": "free flowing",
+            // the day that post is for
+            "day": "friday",
+            // the name for the post on that day
+            "title": "shapes, extrusions, worms, and NURBS",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/11/07",
+            // the theme that post is for
+            "theme": "thundering thicket",
+            // the day that post is for
+            "day": "thursday",
+            // the name for the post on that day
+            "title": "noisy canopys",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/11/06",
+            // the theme that post is for
+            "theme": "wdym",
+            // the day that post is for
+            "day": "wednesday",
+            // the name for the post on that day
+            "title": "spooky skeletons",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/11/05",
+            // the theme that post is for
+            "theme": "scheming",
+            // the day that post is for
+            "day": "tuesday",
+            // the name for the post on that day
+            "title": "daily post idea",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+    ]
+};
+
+export {
+    DAILY_POST_DATA
+};
