765a17c2326b037fcafba6fd256e3a11b6247c0b
Corbeau <aurora.griffith@students.mq.edu.au>
2024-11-09 12:13:00 +1100

    daily post importing moved to module

---
+++ scripts/dailyPost_importing.js
@@ -0,0 +1,50 @@
+
+
+window.addEventListener(
+    "load",
+    (event) => {
+        console.log("importing daily posts");
+        import_all_daily_posts();
+    }
+);
+
+
+
+// ############################################################################################
+// ############################################################################################
+// ############################################################################################
+
+
+function import_all_daily_posts() {
+    var z, i, elmnt, file, xhttp;
+    /* Loop through a collection of all HTML elements: */
+    z = document.getElementsByClassName("daily_post_import");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        /*search for elements with a certain atrribute:*/
+        file = elmnt.getAttribute("import-html");
+        if (file) {
+        /* Make an HTTP request using the attribute value as the file name: */
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+                if (this.readyState == 4) {
+                if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+                if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+                /* Remove the attribute, and call this function once more: */
+                elmnt.removeAttribute("import-html");
+                import_all_daily_posts();
+            }
+        }
+        xhttp.open("GET", file, true);
+        xhttp.send();
+        /* Exit the function: */
+        return;
+        }
+    }
+}
+
+
+
+// ############################################################################################
+// ############################################################################################
+// ############################################################################################
