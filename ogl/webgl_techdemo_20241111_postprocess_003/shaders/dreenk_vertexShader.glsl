fa4a434a4c9642f6e6f9999f84cd24e63aae766a
Corbeau <aurora.griffith@students.mq.edu.au>
2024-11-11 19:56:16 +1100

    glsl files of each shader

---
+++ ogl/webgl_techdemo_20241111_postprocess_003/shaders/dreenk_vertexShader.glsl
@@ -0,0 +1,14 @@
+attribute vec4 aVertexPosition;
+attribute vec2 aTextureCoord;
+
+uniform mat4 uModelMatrix;
+uniform mat4 uViewMatrix;
+uniform mat4 uProjectionMatrix;
+
+varying highp vec2 vTextureCoord;
+
+void main(){
+    vec4 pos = uProjectionMatrix * ( uViewMatrix * (uModelMatrix * aVertexPosition) );
+    gl_Position = pos;
+    vTextureCoord = aTextureCoord;
+}
