392cc0ee85b741feb657fdb486781c1c3686e13a
Corbeau <aurora.griffith@students.mq.edu.au>
2024-11-12 19:39:49 +1100

    smoothing pixel value on y axis

--- ogl/webgl_techdemo_20241111_postprocess_003/shaders/render_fragmentShader.js
+++ ogl/webgl_techdemo_20241111_postprocess_003/shaders/render_fragmentShader.js
@@ -35,24 +35,34 @@ void main() {
   // check for not the bars
   else {
     // remove the bucket amount from the texture uv mapping
-    vec2 uv_mapping = vec2(v_texcoord.x - uv_correction.x, v_texcoord.y - uv_correction.y);
-    // get the true colour
-    vec4 true_texture_colour = texture2D(u_texture, uv_mapping);
+    vec2 channel_uv = vec2(v_texcoord.x - uv_correction.x, v_texcoord.y - uv_correction.y);
+    vec2 uv_mapping_0 = vec2( channel_uv.x, channel_uv.y );
+    vec2 uv_mapping_1 = vec2( channel_uv.x, channel_uv.y+u_uv_pixel_size.y );
+    vec2 uv_mapping_2 = vec2( channel_uv.x, channel_uv.y+(2.0*u_uv_pixel_size.y) );
+    // get the true colours
+    vec4 true_texture_colour_0 = texture2D(u_texture, uv_mapping_0);
+    vec4 true_texture_colour_1 = texture2D(u_texture, uv_mapping_1);
+    vec4 true_texture_colour_2 = texture2D(u_texture, uv_mapping_2);
+    // then average the channel
+    float red_avg = ( true_texture_colour_0.r + true_texture_colour_1.r + true_texture_colour_2.r )/3.0;
+    float green_avg = ( true_texture_colour_0.g + true_texture_colour_1.g + true_texture_colour_2.g )/3.0;
+    float blue_avg = ( true_texture_colour_0.b + true_texture_colour_1.b + true_texture_colour_2.b )/3.0;
+    vec4 true_texture_colour_avg = vec4( red_avg, green_avg, blue_avg, 1.0);
     // ------------------------
     //  when first column of a bucket
     if(bucket_uv.x < 1.0){
       // take only red channel
-      frag_value = vec4( true_texture_colour.r, 0, 0, true_texture_colour.a );
+      frag_value = vec4( true_texture_colour_avg.r, 0, 0, true_texture_colour_avg.a );
     }
     // wasnt first column, might be second column
     else if(bucket_uv.x < 2.0){
       // take only green channel
-      frag_value = vec4( 0, true_texture_colour.g, 0, true_texture_colour.a );
+      frag_value = vec4( 0, true_texture_colour_avg.g, 0, true_texture_colour_avg.a );
     }
     // lastly it must be the 3rd column
     else {
       // take only blue channel
-      frag_value = vec4( 0, 0, true_texture_colour.b, true_texture_colour.a );
+      frag_value = vec4( 0, 0, true_texture_colour_avg.b, true_texture_colour_avg.a );
     }
     // ------------------------
   }
