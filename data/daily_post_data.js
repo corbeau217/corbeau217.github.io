2a3f16d79d72c44ffd6fa2c198b0a7857f960543
Corbeau <aurora.griffith@students.mq.edu.au>
2024-11-14 07:45:17 +1100

    created stubs and fixed template
    fixed pathing in template

--- data/daily_post_data.js
+++ data/daily_post_data.js
@@ -5,6 +5,20 @@ const DAILY_POST_DATA = {
         // ========================================
         {
             // date of the post in YYYY/MM/DD format
+            "date": "2024/11/14",
+            // the theme that post is for
+            "theme": "thundering thicket",
+            // the day that post is for
+            "day": "thursday",
+            // the name for the post on that day
+            "title": "lorem ipsum",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
             "date": "2024/11/13",
             // the theme that post is for
             "theme": "wdym",
