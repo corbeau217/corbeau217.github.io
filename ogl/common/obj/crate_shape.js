04f4ab7ab1fb2c460a05f061496919145e04f43f
Corbeau <aurora.griffith@students.mq.edu.au>
2024-11-22 17:00:08 +1100

    crate shape cooked :D

--- ogl/common/obj/crate_shape.js
+++ ogl/common/obj/crate_shape.js
@@ -18,36 +18,54 @@ export class Crate_Shape {
     build_mesh(){
         this.generate_vertices();
         this.generate_bindings();
+        this.generate_normals();
     }
     generate_vertices(){
         this.vertices = [
-            // left side
+            // ####left side
             // --- bottom
-            -1.0, -1.0, -1.0, 1.0,
             -1.0, -1.0,  1.0, 1.0,
+            -1.0, -1.0, -1.0, 1.0,
             // --- top
-            -1.0,  1.0,  1.0, 1.0,
             -1.0,  1.0, -1.0, 1.0,
+            -1.0,  1.0,  1.0, 1.0,
             // right side
             // --- bottom
-             1.0, -1.0, -1.0, 1.0,
              1.0, -1.0,  1.0, 1.0,
+             1.0, -1.0, -1.0, 1.0,
              // --- top
-             1.0,  1.0,  1.0, 1.0,
              1.0,  1.0, -1.0, 1.0,
+             1.0,  1.0,  1.0, 1.0,
         ];
     }
     generate_bindings(){
         // get our bindings depending on our winding order
         //  diagrams were for anticlockwise winding order
         this.bindings = [
-            // ...
-            // TODO: make bindings lol
+            // left
             0,1,2,
             0,2,1,
+            // back
+            1,5,6,
+            1,6,2,
+            // right
+            5,4,7,
+            5,7,6,
+            // front
+            4,0,3,
+            4,3,7,
+            // top
+            7,3,2,
+            7,2,6,
+            // bottom
+            0,4,5,
+            0,5,1,
         ];
+    }
+    generate_normals(){
+        // ...
     }
     get_vertices(){ return this.vertices; }
     get_bindings(){ return this.bindings; }
-    get_face_count(){ return 2; }
+    get_face_count(){ return 12; }
 }
