e3a63bf050576c0eb17d98a3c03a7cb0ac099b18
Corbeau <aurora.griffith@students.mq.edu.au>
2024-11-28 14:52:32 +1100

    daily post - thursday

--- data/daily_post_data.js
+++ data/daily_post_data.js
@@ -5,6 +5,20 @@ const DAILY_POST_DATA = {
         // ========================================
         {
             // date of the post in YYYY/MM/DD format
+            "date": "2024/11/28",
+            // the theme that post is for
+            "theme": "thundering thicket",
+            // the day that post is for
+            "day": "thursday",
+            // the name for the post on that day
+            "title": "minimalism helps sometimes",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
             "date": "2024/11/27",
             // the theme that post is for
             "theme": "wdym",
