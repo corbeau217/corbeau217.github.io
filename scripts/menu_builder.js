da78767e5509c4309d688b4aea86459f1b55e604
Corbeau <aurora.griffith@students.mq.edu.au>
2024-12-02 01:36:21 +1100

    nav menu construction tweaks

--- scripts/menu_builder.js
+++ scripts/menu_builder.js
@@ -1,24 +1,45 @@
+import { NAV_MENU_ITEMS } from "/data/nav_menu_data.js";
 
-const nav_menu_inner = '<div class="nav_link"><a href="/" class="nav_link_anchor">HOME</a></div>'+
-'<div class="nav_link"><a href="/info/" class="nav_link_anchor">INFO</a></div>'+
-'<div class="nav_link"><a href="/ogl/" class="nav_link_anchor">GRAPHICS</a></div>'+
-'<div class="nav_link"><a href="/daily/" class="nav_link_anchor">DAILY</a></div>';
+/**
+ * makes a nav menu element given a path and label
+ * @param {*} label the label for the nav menu item
+ * @param {*} path the path this nav menu item goes to
+ */
+function nav_menu_element(label,path){
+    return `<div class="nav_link"><a href="${path}" class="nav_link_anchor">${label}</a></div>`
+}
 
+/**
+ * constructs
+ * @returns the constructed content to go within the nav menu
+ */
+function construct_nav_menu_inner_html(){
+    let constructed_elements = NAV_MENU_ITEMS.map(element_data => {
+        return nav_menu_element(element_data.label, element_data.path);
+    });
+    return constructed_elements.join("\n");
+}
 
-function construct_pageNavigation_menu(){
-    // ...
+/**
+ * construct all the items of the nave menu
+ */
+function nav_menu_main(){
+    // gather the nav menu object
     const body_menu_element = document.querySelector("#navMenu");
+
+    // check real
     if(body_menu_element==null) {
         console.log("failed to add the navigation menu to our page");
     }
     else {
-        body_menu_element.innerHTML = nav_menu_inner;
+        // have real element
+        body_menu_element.innerHTML = construct_nav_menu_inner_html();
     }
 }
 
 window.addEventListener(
     "load",
     (event) => {
-        construct_pageNavigation_menu();
+        nav_menu_main();
     }
 );
