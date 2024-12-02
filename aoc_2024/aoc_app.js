0edcb0a5cb3ad66af602758a7bec7429413c3c9a
Corbeau <aurora.griffith@students.mq.edu.au>
2024-12-02 14:56:49 +1100

    aoc day challenges into subfolders

--- aoc_2024/aoc_app.js
+++ aoc_2024/aoc_app.js
@@ -10,80 +10,80 @@ import { AdventOfCode_Page_Manager } from "./aoc_page_manager.js";
 
 // -------------------
 // ----- day_01
-import { code_block as code_block_day_01_1 } from "./blocks/day_01_1.js";
-import { code_block as code_block_day_01_2 } from "./blocks/day_01_2.js";
+import { code_block as code_block_day_01_1 } from "./blocks/day_01/day_01_1.js";
+import { code_block as code_block_day_01_2 } from "./blocks/day_01/day_01_2.js";
 // ----- day_02
-import { code_block as code_block_day_02_1 } from "./blocks/day_02_1.js";
-import { code_block as code_block_day_02_2 } from "./blocks/day_02_2.js";
+import { code_block as code_block_day_02_1 } from "./blocks/day_02/day_02_1.js";
+import { code_block as code_block_day_02_2 } from "./blocks/day_02/day_02_2.js";
 // ----- day_03
-import { code_block as code_block_day_03_1 } from "./blocks/day_03_1.js";
-import { code_block as code_block_day_03_2 } from "./blocks/day_03_2.js";
+import { code_block as code_block_day_03_1 } from "./blocks/day_03/day_03_1.js";
+import { code_block as code_block_day_03_2 } from "./blocks/day_03/day_03_2.js";
 // ----- day_04
-import { code_block as code_block_day_04_1 } from "./blocks/day_04_1.js";
-import { code_block as code_block_day_04_2 } from "./blocks/day_04_2.js";
+import { code_block as code_block_day_04_1 } from "./blocks/day_04/day_04_1.js";
+import { code_block as code_block_day_04_2 } from "./blocks/day_04/day_04_2.js";
 // ----- day_05
-import { code_block as code_block_day_05_1 } from "./blocks/day_05_1.js";
-import { code_block as code_block_day_05_2 } from "./blocks/day_05_2.js";
+import { code_block as code_block_day_05_1 } from "./blocks/day_05/day_05_1.js";
+import { code_block as code_block_day_05_2 } from "./blocks/day_05/day_05_2.js";
 // ----- day_06
-import { code_block as code_block_day_06_1 } from "./blocks/day_06_1.js";
-import { code_block as code_block_day_06_2 } from "./blocks/day_06_2.js";
+import { code_block as code_block_day_06_1 } from "./blocks/day_06/day_06_1.js";
+import { code_block as code_block_day_06_2 } from "./blocks/day_06/day_06_2.js";
 // ----- day_07
-import { code_block as code_block_day_07_1 } from "./blocks/day_07_1.js";
-import { code_block as code_block_day_07_2 } from "./blocks/day_07_2.js";
+import { code_block as code_block_day_07_1 } from "./blocks/day_07/day_07_1.js";
+import { code_block as code_block_day_07_2 } from "./blocks/day_07/day_07_2.js";
 // ----- day_08
-import { code_block as code_block_day_08_1 } from "./blocks/day_08_1.js";
-import { code_block as code_block_day_08_2 } from "./blocks/day_08_2.js";
+import { code_block as code_block_day_08_1 } from "./blocks/day_08/day_08_1.js";
+import { code_block as code_block_day_08_2 } from "./blocks/day_08/day_08_2.js";
 // ----- day_09
-import { code_block as code_block_day_09_1 } from "./blocks/day_09_1.js";
-import { code_block as code_block_day_09_2 } from "./blocks/day_09_2.js";
+import { code_block as code_block_day_09_1 } from "./blocks/day_09/day_09_1.js";
+import { code_block as code_block_day_09_2 } from "./blocks/day_09/day_09_2.js";
 // ----- day_10
-import { code_block as code_block_day_10_1 } from "./blocks/day_10_1.js";
-import { code_block as code_block_day_10_2 } from "./blocks/day_10_2.js";
+import { code_block as code_block_day_10_1 } from "./blocks/day_10/day_10_1.js";
+import { code_block as code_block_day_10_2 } from "./blocks/day_10/day_10_2.js";
 // ----- day_11
-import { code_block as code_block_day_11_1 } from "./blocks/day_11_1.js";
-import { code_block as code_block_day_11_2 } from "./blocks/day_11_2.js";
+import { code_block as code_block_day_11_1 } from "./blocks/day_11/day_11_1.js";
+import { code_block as code_block_day_11_2 } from "./blocks/day_11/day_11_2.js";
 // ----- day_12
-import { code_block as code_block_day_12_1 } from "./blocks/day_12_1.js";
-import { code_block as code_block_day_12_2 } from "./blocks/day_12_2.js";
+import { code_block as code_block_day_12_1 } from "./blocks/day_12/day_12_1.js";
+import { code_block as code_block_day_12_2 } from "./blocks/day_12/day_12_2.js";
 // ----- day_13
-import { code_block as code_block_day_13_1 } from "./blocks/day_13_1.js";
-import { code_block as code_block_day_13_2 } from "./blocks/day_13_2.js";
+import { code_block as code_block_day_13_1 } from "./blocks/day_13/day_13_1.js";
+import { code_block as code_block_day_13_2 } from "./blocks/day_13/day_13_2.js";
 // ----- day_14
-import { code_block as code_block_day_14_1 } from "./blocks/day_14_1.js";
-import { code_block as code_block_day_14_2 } from "./blocks/day_14_2.js";
+import { code_block as code_block_day_14_1 } from "./blocks/day_14/day_14_1.js";
+import { code_block as code_block_day_14_2 } from "./blocks/day_14/day_14_2.js";
 // ----- day_15
-import { code_block as code_block_day_15_1 } from "./blocks/day_15_1.js";
-import { code_block as code_block_day_15_2 } from "./blocks/day_15_2.js";
+import { code_block as code_block_day_15_1 } from "./blocks/day_15/day_15_1.js";
+import { code_block as code_block_day_15_2 } from "./blocks/day_15/day_15_2.js";
 // ----- day_16
-import { code_block as code_block_day_16_1 } from "./blocks/day_16_1.js";
-import { code_block as code_block_day_16_2 } from "./blocks/day_16_2.js";
+import { code_block as code_block_day_16_1 } from "./blocks/day_16/day_16_1.js";
+import { code_block as code_block_day_16_2 } from "./blocks/day_16/day_16_2.js";
 // ----- day_17
-import { code_block as code_block_day_17_1 } from "./blocks/day_17_1.js";
-import { code_block as code_block_day_17_2 } from "./blocks/day_17_2.js";
+import { code_block as code_block_day_17_1 } from "./blocks/day_17/day_17_1.js";
+import { code_block as code_block_day_17_2 } from "./blocks/day_17/day_17_2.js";
 // ----- day_18
-import { code_block as code_block_day_18_1 } from "./blocks/day_18_1.js";
-import { code_block as code_block_day_18_2 } from "./blocks/day_18_2.js";
+import { code_block as code_block_day_18_1 } from "./blocks/day_18/day_18_1.js";
+import { code_block as code_block_day_18_2 } from "./blocks/day_18/day_18_2.js";
 // ----- day_19
-import { code_block as code_block_day_19_1 } from "./blocks/day_19_1.js";
-import { code_block as code_block_day_19_2 } from "./blocks/day_19_2.js";
+import { code_block as code_block_day_19_1 } from "./blocks/day_19/day_19_1.js";
+import { code_block as code_block_day_19_2 } from "./blocks/day_19/day_19_2.js";
 // ----- day_20
-import { code_block as code_block_day_20_1 } from "./blocks/day_20_1.js";
-import { code_block as code_block_day_20_2 } from "./blocks/day_20_2.js";
+import { code_block as code_block_day_20_1 } from "./blocks/day_20/day_20_1.js";
+import { code_block as code_block_day_20_2 } from "./blocks/day_20/day_20_2.js";
 // ----- day_21
-import { code_block as code_block_day_21_1 } from "./blocks/day_21_1.js";
-import { code_block as code_block_day_21_2 } from "./blocks/day_21_2.js";
+import { code_block as code_block_day_21_1 } from "./blocks/day_21/day_21_1.js";
+import { code_block as code_block_day_21_2 } from "./blocks/day_21/day_21_2.js";
 // ----- day_22
-import { code_block as code_block_day_22_1 } from "./blocks/day_22_1.js";
-import { code_block as code_block_day_22_2 } from "./blocks/day_22_2.js";
+import { code_block as code_block_day_22_1 } from "./blocks/day_22/day_22_1.js";
+import { code_block as code_block_day_22_2 } from "./blocks/day_22/day_22_2.js";
 // ----- day_23
-import { code_block as code_block_day_23_1 } from "./blocks/day_23_1.js";
-import { code_block as code_block_day_23_2 } from "./blocks/day_23_2.js";
+import { code_block as code_block_day_23_1 } from "./blocks/day_23/day_23_1.js";
+import { code_block as code_block_day_23_2 } from "./blocks/day_23/day_23_2.js";
 // ----- day_24
-import { code_block as code_block_day_24_1 } from "./blocks/day_24_1.js";
-import { code_block as code_block_day_24_2 } from "./blocks/day_24_2.js";
+import { code_block as code_block_day_24_1 } from "./blocks/day_24/day_24_1.js";
+import { code_block as code_block_day_24_2 } from "./blocks/day_24/day_24_2.js";
 // ----- day_25
-import { code_block as code_block_day_25_1 } from "./blocks/day_25_1.js";
-import { code_block as code_block_day_25_2 } from "./blocks/day_25_2.js";
+import { code_block as code_block_day_25_1 } from "./blocks/day_25/day_25_1.js";
+import { code_block as code_block_day_25_2 } from "./blocks/day_25/day_25_2.js";
 // -------------------
 
 // ############################################################################################
