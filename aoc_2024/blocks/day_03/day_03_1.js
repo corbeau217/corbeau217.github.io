c4dcee219cfb4a8528f1bc5a595b6ef8dc4aff95
Corbeau <aurora.griffith@students.mq.edu.au>
2024-12-03 17:21:52 +1100

    aoc part 1

--- aoc_2024/blocks/day_03/day_03_1.js
+++ aoc_2024/blocks/day_03/day_03_1.js
@@ -34,6 +34,24 @@ export function code_block( raw_input_data ){
     const data_block = prep_data(raw_input_data);
     // ==============================
 
+    const MATCH_LITERAL_NUMBER = /[1-9][0-9]{0,2}/gm;
+    const MATCH_MUL_BLOCK = /mul\([1-9][0-9]{0,2},[1-9][0-9]{0,2}\)/gm;
+    
+    // gather legals
+    let legal_mul_blocks = raw_input_data.match(MATCH_MUL_BLOCK);
+
+
+    // go through them all
+    for (let legal_mul_index = 0; legal_mul_index < legal_mul_blocks.length; legal_mul_index++) {
+        const current_mul_expression = legal_mul_blocks[legal_mul_index];
+        // ------------------------------
+        // get the numbers
+        let current_nums = current_mul_expression.match(MATCH_LITERAL_NUMBER);
+        // ------------------------------
+        answer += Number(current_nums[0])*Number(current_nums[1]);
+    }
+
+    // do match
     // TODO:  do the day
 
     // ==============================
