07956b98333209597d40e87b828f51abbb0b89fd
Corbeau <aurora.griffith@students.mq.edu.au>
2024-12-07 09:42:43 +1100

    partial working on aoc2024 - d6p1

--- aoc_2024/blocks/day_06/day_06_1.js
+++ aoc_2024/blocks/day_06/day_06_1.js
@@ -32,10 +32,138 @@ export function code_block( raw_input_data ){
      * ```
      */
     const data_block = prep_data(raw_input_data);
+    const DIRECTION_COUNT = 4;
+    const HASH_CHAR = '#';
+    const COLUMN_COUNT = data_block.lines_without_empty.character_grid[0].length;
+    const ROW_COUNT = data_block.lines_without_empty.character_grid.length;
+    // ==============================
+    let end_lists = {
+        column: {
+            left: [],
+            right: [],
+        },
+        row: {
+            top: [],
+            bottom: [],
+        }
+    }
+    // ==============================
+    /**
+     * make end lists 
+     */
+    let initialise_end_lists = ()=>{
+        // all rows
+        for (let y_index = 0; y_index < ROW_COUNT; y_index++) {
+            end_lists.row.top.push(new Map());
+            end_lists.row.bottom.push(new Map());
+        }
+        // all columns
+        for (let x_index = 0; x_index < COLUMN_COUNT; x_index++) {
+            end_lists.row.top.push(new Map());
+            end_lists.row.bottom.push(new Map());
+        }
+    };
+    /**
+     * make end lists and then add to them
+     * @param {*} character_grid 
+     */
+    let fill_end_lists = (character_grid)=>{
+        // all rows
+        for (let y_index = 0; y_index < ROW_COUNT; y_index++) {
+            const grid_row = character_grid[y_index];
+            // all columns in row
+            for (let x_index = 0; x_index < COLUMN_COUNT; x_index++) {
+                const current_character = grid_row[x_index];
+                // found hash
+                if (current_character === HASH_CHAR) {
+                    // add it in all directions
+                    for (let direction = 0; direction < DIRECTION_COUNT; direction++) {
+                        add_hash_to_list(x_index,y_index,direction);
+                    }
+                }
+            }
+        }
+    };
+    initialise_end_lists();
+    fill_end_lists(data_block.lines_without_empty.character_grid);
+    // ==============================
+    /**
+     * given a location of a hash, it gets added to the list at the end of the rows/columns
+     * @param {*} hash_x_index 
+     * @param {*} hash_y_index 
+     * @param {*} direction 
+     */
+    let add_hash_to_list = (hash_x_index, hash_y_index, direction)=>{
+        // TODO: add to list
+    };
+    // ==============================
+    /**
+     * test location for legality
+     * @param {*} x_index 
+     * @param {*} y_index 
+     * @returns 
+     */
+    let is_legal_indices=(x_index,y_index)=>{
+        return (x_index >= 0 && x_index < COLUMN_COUNT) && (y_index >= 0 && y_index < ROW_COUNT);
+    };
+    // ==============================
+    let is_direction_vertical=(direction)=>{ return direction%2==0; };
+    let is_direction_horizontal=(direction)=>{ return direction%2!=0; };
+    /**
+     * get the end list by direction
+     * * `0` - UP
+     * * `1` - RIGHT
+     * * `2` - DOWN
+     * * `3` - LEFT
+     * @param {*} direction in range: [0,1,2,3]
+     * @returns 
+     */
+    let get_end_list_by_direction=(direction)=>{
+        switch (direction) {
+            default:
+            case 0: return end_lists.row.top;
+            case 1: return end_lists.column.right;
+            case 2: return end_lists.row.bottom;
+            case 3: return end_lists.column.left;
+        }
+    };
+    // ==============================
+    /**
+     * test if already handled all the collisions
+     * @param {*} x_index 
+     * @param {*} y_index 
+     * @param {*} direction 
+     * @param {*} end_list 
+     */
+    let has_more_collisions=(x_index,y_index,direction,end_list)=>{
+        // TODO: ...
+    }
+    let has_more_collisions_up=(x_index,y_index,end_list)=>{
+        // TODO: ...
+    }
+    let has_more_collisions_right=(x_index,y_index,end_list)=>{
+        // TODO: ...
+    }
+    let has_more_collisions_down=(x_index,y_index,end_list)=>{
+        // TODO: ...
+    }
+    let has_more_collisions_left=(x_index,y_index,end_list)=>{
+        // TODO: ...
+    }
+    // ==============================
+    /**
+     * gather the location it will collide with a hash
+     * @param {*} x_index 
+     * @param {*} y_index 
+     * @param {*} direction 
+     */
+    let get_collision_location_by_indices_and_direction = (x_index, y_index, direction)=>{
+        // TODO: 
+    };
     // ==============================
 
     // TODO:  do the day
-    answer = "unfinished";
+    answer = "in-progress";
 
     // ==============================
     return answer;
